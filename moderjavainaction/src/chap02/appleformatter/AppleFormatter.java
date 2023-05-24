7c99267b0cfe7500b06fdbf006104338d46a8db4
BeomSeogKim <kbs4520@daum.net>
2023-05-24 13:36:37 +0900

    재시작

--- moderjavainaction/src/chap02/appleformatter/AppleFormatter.java
+++
@@ -1,7 +0,0 @@
-package chap02.appleformatter;
-
-import chap02.FilteringApples;
-
-public interface AppleFormatter {
-    String accept(FilteringApples.Apple apple);
-}
--- moderjavainaction/src/chap02/applepredicate/ApplePredicate.java
+++
@@ -1,7 +0,0 @@
-package chap02.applepredicate;
-
-import chap02.FilteringApples;
-
-public interface ApplePredicate {
-    boolean test(FilteringApples.Apple apple);
-}
